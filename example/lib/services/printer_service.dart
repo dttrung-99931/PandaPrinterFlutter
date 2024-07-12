d6f9a602cd14722c187803e9b172a243c8e718e8
Đoàn Thanh Trung <doanthanhtrung4321@gmail.com>
2024-07-12 09:27:07 +0700

    [android] fix android native project error

--- example/lib/services/printer_service.dart
+++ example/lib/services/printer_service.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
@@ -8,9 +7,7 @@ import 'package:panda_print_plugin/panda_print.dart';
 import 'package:panda_printer_example/models/app_error.dart';
 import 'package:panda_printer_example/models/printer_model.dart';
 import 'package:panda_printer_example/utils/ble_utils.dart';
-import 'package:panda_printer_example/utils/extensions/async_extension.dart';
 import 'package:panda_printer_example/utils/extensions/list_extension.dart';
-import 'package:print_plugin/print_plugin.dart';
 
 class PrinterService extends ChangeNotifier {
   PrinterService() {
@@ -22,19 +19,19 @@ class PrinterService extends ChangeNotifier {
   late Completer<bool> _printSuccessCompleter;
 
   void setListener() {
-    PrintPlugin.listenChannelEvent(onPrintSuccess: () {
-      log('print success');
-      _printSuccessCompleter.completeIfNot(true);
-    }, onPrintFail: () {
-      log('print failed');
-      _printSuccessCompleter.completeIfNot(false);
-    }, onConnectSuccess: () {
-      log('Connect sucess');
-    }, onConnectFail: () {
-      log('Connect failed');
-    }, onDisconnected: () {
-      log('Disconnected');
-    });
+    // PrintPlugin.listenChannelEvent(onPrintSuccess: () {
+    //   log('print success');
+    //   _printSuccessCompleter.completeIfNot(true);
+    // }, onPrintFail: () {
+    //   log('print failed');
+    //   _printSuccessCompleter.completeIfNot(false);
+    // }, onConnectSuccess: () {
+    //   log('Connect sucess');
+    // }, onConnectFail: () {
+    //   log('Connect failed');
+    // }, onDisconnected: () {
+    //   log('Disconnected');
+    // });
   }
 
   Future<Either<AppError, List<PrinterModel>>> lookUpPrinters() async {
@@ -47,20 +44,23 @@ class PrinterService extends ChangeNotifier {
   }
 
   Future<Either<AppError, void>> connectPrinter(PrinterModel printer) async {
-    bool success = await PrintPlugin.connectBle(printer.address);
-    return success
-        ? const Right(null) // null for void
-        : Left(PrinterError(message: 'Connect to printer ${printer.address} failed'));
+    // bool success = await PrintPlugin.connectBle(printer.address);
+    // return success
+    //     ? const Right(null) // null for void
+    //     : Left(PrinterError(message: 'Connect to printer ${printer.address} failed'));
+    return Right(null);
   }
 
   Future<Either<AppError, void>> print() async {
-    _printSuccessCompleter = Completer();
-    PrintPlugin.printerFilter('aaa');
-    // PrintPlugin.printQrCodeLogin({
-    //   'title': 'Test',
-    //   'data': 'Test',
-    //   'content': 'Test',
-    // });
-    return await _printSuccessCompleter.future ? const Right(null) : Left(PrinterError(message: 'Print failed'));
+    return Right(null);
+
+    // _printSuccessCompleter = Completer();
+    // PrintPlugin.printerFilter('aaa');
+    // // PrintPlugin.printQrCodeLogin({
+    // //   'title': 'Test',
+    // //   'data': 'Test',
+    // //   'content': 'Test',
+    // // });
+    // return await _printSuccessCompleter.future ? const Right(null) : Left(PrinterError(message: 'Print failed'));
   }
 }
